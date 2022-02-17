3d1c1b250a2437d6bee1ac16593d49dac93518ab
hjh <hjiahao9527@gmail.com>
2022-02-17 16:56:42 +0800

    Add static files

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-# print(1)
+# print(1) https://github.com/greyli/flask-tutorial/blob/master/chapters
 
+# chapter 2 Hello https://github.com/greyli/flask-tutorial/blob/master/chapters/c2-hello.md
 # http://127.0.0.1:5000   http://localhost:5000 
+'''
 from flask import Flask
 app = Flask(__name__)
 
@@ -29,3 +31,51 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+    
+'''
+
+# chapter 3 templates https://github.com/greyli/flask-tutorial/blob/master/chapters/c3-template.md
+'''
+name = 'hhhhhh'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+'''
+
+# chapter 4 Static https://github.com/greyli/flask-tutorial/blob/master/chapters/c4-static.md
+name = 'hhhhhh'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
